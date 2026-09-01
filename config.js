8171148c0619f0a9a5e249a7d6bc6f115dd767ce
Ranabrand36 <Ranabrand36@users.noreply.github.com>
2026-09-01 07:33:11 +0000

    Upload project archive unchanged

---
+++ config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+    mainToken: '8507456465:AAG84SHfw1reo4KRYo1oP2RE0F7Cqbdsznw', // Bot Token Here 
+    
+    S7: 'LEGEND HASSAN', // Owner, Name
+    
+    adminId: '7537709881', // Owner Chat ID ( control /address, /delresell )
+    
+    channel: 'https://t.me/legendhassanchannel', // TG channel link
+    
+    group: 'https://t.me/legendotpgroup', // TG group link
+    
+    bot: 'LEGEND BUG BOT', // Bot name 
+    
+    logo: './SY/Loves.jpg' // Bot logo path or URL
+};
